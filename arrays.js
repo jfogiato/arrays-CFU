1140b7b5731c4fa551492160d6c2e746aeee18b0
Joe Fogiato <joe.fogiato@gmail.com>
2022-11-09 15:33:47 -0500

    Add an explanation of my understanding of indeces

--- arrays.js
+++ arrays.js
@@ -10,3 +10,6 @@ friendAges.shift()
 
 // Since Cole is getting married next year, the splice command changes his marital status from "false" to "true". He is at index 2, "1" tells the splice method to replace that value, and "true" is the value we are replacing it with.  
 friendMarrieds.splice(2, 1, true)
+
+
+// I accidentally demonstrated my understanding of index positions in the splice() method above! Knowing that I'm trying to replace Cole's marital status with "true", I used the index of 2 to access the third element in that array. 
